583348b5389e2366760d19498482b88d534e2a56
Stéphane Mourey <s+github@stephanemourey.fr>
2023-11-08 22:53:20 +0100

    Uniformize logo to serif font

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
-        <span class="text-8xl inline-block ">T</span><span class="text-7xl inline-block relative -left-7">f</span>
+    <div class="font-serif">
+        <span class="text-7xl inline-block ">T</span><span class="text-[3.1rem] inline-block relative -left-5">f</span>
     </div>
 </template>
