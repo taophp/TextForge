df54adbb4892f3285e259296c97d632c9c2db7b6
Stéphane Mourey <mail@stephanemourey.fr>
2024-02-22 22:20:58 +0100

    Fix: Texts do not use Factory

--- app/Models/Text.php
+++ app/Models/Text.php
@@ -3,13 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Text extends Model
 {
     use HasUlids;
-    use HasFactory;
     protected $keyType = 'string';
     public $incrementing = false;
 
