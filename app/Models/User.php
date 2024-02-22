1de5e6cb27def810729aaa7c2ee93e4e837ccd7c
Stéphane Mourey <mail@stephanemourey.fr>
2024-02-22 22:10:55 +0100

    Reverse the user containers relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Container;
 
 class User extends Authenticatable
 {
@@ -46,4 +48,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function containers(): HasMany
+    {
+        return $this->hasMany(Container::class);
+    }
 }
